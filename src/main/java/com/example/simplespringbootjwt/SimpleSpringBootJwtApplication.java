1c7f184b656e4203cf0d4ce4410f9c98be093ab0
bbyoda <h2trungvuong@gmail.com>
2023-01-07 12:10:11 +0100

    feat: init springboot project

---
+++ src/main/java/com/example/simplespringbootjwt/SimpleSpringBootJwtApplication.java
@@ -0,0 +1,16 @@
+package com.example.simplespringbootjwt;
+
+import com.example.controller.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages ="com.example")
+public class SimpleSpringBootJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringBootJwtApplication.class, args);
+    }
+
+}
